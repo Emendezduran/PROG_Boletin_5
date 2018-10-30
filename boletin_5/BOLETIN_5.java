f4c7745cb7755855b4f74b2bd890177cd37b1033
emendezduran <emendezduran@nautilus25.danielcastelao.org>
2018-10-30 10:53:53 +0100

    commit final

--- boletin_5/BOLETIN_5.java
+++ boletin_5/BOLETIN_5.java
@@ -8,19 +8,15 @@ import java.util.Scanner;
  */
 public class BOLETIN_5 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
+        public static void main(String[] args) {
 
         //Cuenta1    
         System.out.println("Teclee el nombre del titular:");
         Scanner scanNO = new Scanner(System.in);
         String nombre = scanNO.next();
 
-        System.out.println("Con cuanto dinero abriremos la cuenta?:");
-        Scanner scanSA = new Scanner(System.in);
-        Double saldo = scanSA.nextDouble();
+        System.out.println(nombre + "le regalamos 100€ por abrir su cuenta:");
+        Double saldo = 100d;
 
         int numeroCuenta = Cuenta.codCuenta;
 
@@ -30,23 +26,52 @@ public class BOLETIN_5 {
         //Cuenta2
         System.out.println("Teclee el nombre del titular:");
         Scanner scanNO2 = new Scanner(System.in);
-        String nombre2 = scanNO.next();
+        String nombre2 = scanNO2.next();
 
-        System.out.println("Con cuanto dinero abriremos la cuenta?:");
-        Scanner scanSA2 = new Scanner(System.in);
-        Double saldo2 = scanSA.nextDouble();
+        System.out.println(nombre2 + "le regalamos 100€ por abrir su cuenta:");
+        Double saldo2 = 100d;
 
         int numeroCuenta2 = Cuenta.codCuenta;
 
         Cuenta cuenta2 = new Cuenta(nombre2, numeroCuenta2, saldo2);
-        cuenta1.visualizar();
+        cuenta2.visualizar();
         
+         //Cuenta3
+        System.out.println("Teclee el nombre del titular:");
+        Scanner scanNO3 = new Scanner(System.in);
+        String nombre3 = scanNO3.next();
 
-        cuenta1.trasferencia(cuenta2, 100);
+        System.out.println(nombre3 + "le regalamos 100€ por abrir su cuenta:");
+        Double saldo3 = 100d;
 
+        int numeroCuenta3 = Cuenta.codCuenta;
+
+        Cuenta cuenta3 = new Cuenta(nombre3, numeroCuenta3, saldo3);
+        cuenta3.visualizar();
+        
+        cuenta1.setIngreso(900);
+        cuenta2.setIngreso(700);
+        cuenta3.setIngreso(600);
+        
         cuenta1.visualizar();
         cuenta2.visualizar();
-
+        cuenta3.visualizar();
+        
+        cuenta1.setReintegro(50);
+        cuenta2.setReintegro(50);
+        cuenta3.setReintegro(50);
+        
+        cuenta1.visualizar();
+        cuenta2.visualizar();
+        cuenta3.visualizar();
+        
+        cuenta1.trasferencia(cuenta3, 25);
+        cuenta2.trasferencia(cuenta3, 25);
+        cuenta3.trasferencia(cuenta2, 5);
+        
+        cuenta1.visualizar();
+        cuenta2.visualizar();
+        cuenta3.visualizar(); 
     }
 
 }
--- boletin_5/Cuenta.java
+++ boletin_5/Cuenta.java
@@ -1,4 +1,3 @@
-
 package boletin_5;
 
 /**
@@ -6,70 +5,147 @@ package boletin_5;
  * @author emendezduran
  */
 public class Cuenta {
-    
+//Clase Cuenta
+
+    /**
+     * nombre del cliente
+     */
     private String nombreDelCliente;
-    private static int numeroDeCuenta;
+    /**
+     * numero de una cuenta
+     */
+    private int numeroDeCuenta;
+    /**
+     * Saldo de una cuenta
+     */
     private double saldo;
+    /**
+     *
+     */
     public static int codCuenta = 1000;
-            
-    public Cuenta (){
-    numeroDeCuenta = ++ codCuenta; 
+
+    /**
+     * Constructor sin parametros, se le asigna un numeroDeCuenta unico
+     */
+    public Cuenta() {
+        numeroDeCuenta = ++codCuenta;
     }
-    
-    public Cuenta (String  nombreDelCliente, int numeroDeCuenta, double saldo){
-        this.nombreDelCliente= nombreDelCliente;
-        this.saldo= saldo;   
-        this.numeroDeCuenta = ++ codCuenta;
-        
-    }    
-    
-    public void setNombreCliente(String nombreDelCliente){
-    this.nombreDelCliente= nombreDelCliente;
-    }   
-    
-    public void setNumeroCuenta(int numeroDeCuenta){ 
-    this.numeroDeCuenta= numeroDeCuenta;
+
+    /**
+     * Constructor para crear cuenta corriente
+     *
+     * @param nombreDelCliente define el nombre del usuario
+     * @param numeroDeCuenta define el número de cuenta generado por el programa
+     * @param saldo define el saldo con el que se abre la cuenta
+     */
+    public Cuenta(String nombreDelCliente, int numeroDeCuenta, double saldo) {
+        this.nombreDelCliente = nombreDelCliente;
+        this.saldo = saldo;
+        this.numeroDeCuenta = ++codCuenta;
+
     }
-    
-    public void setIngreso(double sumar){   
-    saldo = saldo + sumar;
+
+    /**
+     * Metodo para Cambiar el nombre del cliente
+        * @param nombreDelCliente define el nombre del usuario
+     */
+    public void setNombreCliente(String nombreDelCliente) {
+        this.nombreDelCliente = nombreDelCliente;
     }
-    
-    public void setReintegro(double restar){   
-        if (restar<=0){
-        System.out.println("ERROR");
+
+    /**
+     * Metodo para modificar el numero de cuenta
+     *
+     * @param numeroDeCuenta define el número de cuenta
+     * @deprecated
+     */
+    public void setNumeroCuenta(int numeroDeCuenta) {
+        this.numeroDeCuenta = numeroDeCuenta;
+    }
+
+    /**
+     * Metodo para realizar un ingreso
+     *
+     * @param sumar define el monto de ingreso
+     */
+    public void setIngreso(double sumar) {
+        System.out.println("Efectuando Ingreso...");
+        saldo = saldo + sumar;
+    }
+
+    /**
+     * Metodo para realizar un reintegro, verifica el saldo disponible y el
+     * monto para no ser negativo
+     *
+     * @param restar define el monto de reintegro
+     */
+    public void setReintegro(double restar) {
+        if (restar <= 0) {
+            System.out.println("ERROR");
         }
-        if (saldo<restar){
-        System.out.println("Saldo Insuficiente");
+        if (saldo < restar) {
+            System.out.println("Saldo Insuficiente");
+        } else {
+            System.out.println("Efectuando Reintegro...");
+            saldo = saldo - restar;
         }
-        else{
-        System.out.println("Efectuando Operacion...");
-        saldo = saldo - restar;
+    }
+
+    /**
+     * Metodo para llamar al parametro nombreCliente
+     *
+     * @return nombreDelCliente
+     */
+    public String getNombreCliente() {
+        return nombreDelCliente;
+    }
+
+    /**
+     * Metodo para llamar al parametro numeroDeCuenta
+     *
+     * @return numeroDeCuenta
+     */
+    public int getNumeroCuenta() {
+        return numeroDeCuenta;
+    }
+
+    /**
+     * Metodo para llamar al parametro saldo
+     *
+     * @return saldo
+     */
+    public double getSaldo() {
+        return saldo;
+    }
+
+    /**
+     * Metodo para visualizar los datos de una cuenta
+     *
+     */
+    public void visualizar() {
+        System.out.println("Nombre: " + nombreDelCliente + " Numero de cuenta: " + numeroDeCuenta + " Saldo Disponible: " + saldo);
+    }
+
+    /**
+     * Metodo para realizar una transferencia, verifica el saldo disponible y el
+     * monto para no ser negativo
+     *
+     * @param cuentaDest define la cuenta que recibe el importe
+     * @param imp define el importe a transferir
+     */
+    public void trasferencia(Cuenta cuentaDest, double imp) {
+        Cuenta cuentaDestino = cuentaDest;
+        double importe = imp;
+        if (imp <= 0) {
+            System.out.println("ERROR");
+        }
+        if (saldo < imp) {
+            System.out.println("Saldo Insuficiente");
+        } else {
+            System.out.println("Efectuando Transferencia...");
+            saldo -= importe;
+            cuentaDest.setIngreso(importe);
         }
     }
-    
-    public String getNombreCliente(){ 
-    return nombreDelCliente;
-    }   
-    
-    public int getNumeroCuenta(){ 
-    return numeroDeCuenta;
-    }
-    
-    public double getSaldo(){ 
-    return saldo;
-    }
-    
-    public void visualizar(){
-    System.out.println("Nombre: " + nombreDelCliente + "Numero de cuenta: " + numeroDeCuenta + "Saldo Disponible: " + saldo);       
-    }
-       
-    
-    public void trasferencia(Cuenta cuentaDest, double imp){
-    Cuenta cuentaDestino = cuentaDest;     
-    double importe = imp; 
-    saldo -= importe;
-    cuentaDest.setIngreso(importe);
-    }
-                         
+
 }
